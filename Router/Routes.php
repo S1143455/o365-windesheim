f36ad0e37d716168d2d543692154837bdf0bbee3
Niels Meijer <s1144379@student.windesheim.nl>
2019-11-29 16:34:07 +0100

    Veranderingen: Toevoeging side-bar, default pagina's, navigatie admin pagina

--- Router/Routes.php
+++ Router/Routes.php
@@ -23,8 +23,37 @@ $router->route('/admin', function(){
     return include 'content/backend/home-admin.php';
 });
 
+$router->route('/admin/onderhoud', function(){
+    return include 'content/backend/onderhoud-admin.php';
+});
 
+$router->route('/admin/onderhoud-hoofdpagina', function(){
+    return include 'content/backend/onderhoudhoofdpagina.php';
+});
 
+$router->route('/admin/onderhoud-categorieen', function(){
+    return include 'content/backend/onderhoudc.php';
+});
+
+$router->route('/admin/onderhoud-producten', function(){
+    return include 'content/backend/onderhoudproducten.php';
+});
+
+$router->route('/admin/onderhoud-klanten', function(){
+    return include 'content/backend/onderhoudklanten.php';
+});
+
+$router->route('/admin/onderhoud-korting', function(){
+    return include 'content/backend/onderhoudkorting.php';
+});
+
+$router->route('/admin/onderhoud-nieuwsbrief', function(){
+    return include 'content/backend/onderhoudnieuwsbrief.php';
+});
+
+$router->route('/admin/bestellingoverzicht', function(){
+    return include 'content/backend/bestellingoverzicht.php';
+});
 
 $router->resource('/product', 'product');
 
---
+++ content/backend/sidebar-admin.php
@@ -0,0 +1,10 @@
+
+<div class="sidenav">
+
+
+    <?php
+    echo $admin->nav_menu();
+
+    ?>
+</div>
+
