00d74f07e83adc8c4444fc1e85e6ea1bc7b879d1
Sebastiaan Leeflang <s.leeflang@hotmail.com>
2019-12-02 22:50:48 +0100

    include teveel

--- content/backend/header-admin.php
+++ content/backend/header-admin.php
@@ -17,7 +17,6 @@ $admin = new Controller\Admin();
     <link href="<?php echo $main->template_path() ?>admin.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo $main->template_path() ?>custom.css" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 </head>
