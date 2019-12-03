35297f4df7cd8d8896bc7b108ffbe1861d5e3890
S1147103 <S1147103@student.windesheim.com>
2019-12-03 20:02:39 +0100

    User example
    readded gulpfile example aswell

--- Model/User.php
+++ Model/User.php
@@ -4,7 +4,49 @@
 namespace Model;
 
 
-class User
+class User extends Database
 {
+    private $username;
+    private $password;
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+    public function checkCredentials()
+    {
+        'SELECT * FROM people WHERE username = ? or email = ? ';
+        if($sqlreturendsomething)
+            $this->setPassword($retrieved->password)
+
+        return true;
+    }
 
 }
