eb292d487dbd7ebab5dc59cfb21a779306f12ee2
Sven <pluis29@yahoo.com>
2019-12-06 11:14:00 +0100

    voor de login

--- Model/User.php
+++ Model/User.php
@@ -49,7 +49,6 @@ class User extends Database
 
     public function checkCredentials()
     {
-<<<<<<< HEAD
         $getthedata=new Database();
         $sqlreturendsomething=$getthedata->select("SELECT * FROM people WHERE LogonName = '".$this->username. "'");
         print_r($sqlreturendsomething);
@@ -58,13 +57,6 @@ class User extends Database
             $_SESSION['LOGIN_ERROR']='Gebruikersnaam of wachtwoord onjuist.';
             return;
         }
-=======
-        'SELECT * FROM people WHERE username = ? or email = ? ';
-        if($sqlreturendsomething)
-            $this->setPassword($retrieved->password);
-
-        return true;
->>>>>>> 176adc9769a05f7262d4d6cf760caeaaa6f35f92
     }
 
 }
