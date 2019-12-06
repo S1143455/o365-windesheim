07c1471fe68bcdedfae5cf5d908523feff53702d
S1147103 <S1147103@student.windesheim.com>
2019-12-06 15:43:55 +0100

    Generic model stuff

--- Model/Models.php
+++ Model/Models.php
@@ -17,20 +17,28 @@ class Models
 
     protected function getColumns()
     {
-        switch ($this->table) {
-            case "stockitem":
-                $this->getStockItem();
-                break;
-            case 'category':
-                $this->getCategory();
-                break;
-            case 'attachment':
-                $this->getAttachments();
-                break;
-            default:
-                die('Table not implemented');
+        if($this->column == null)
+        {
+            switch ($this->table) {
+                case "stockitem":
+                    $this->getStockItem();
+                    break;
+                case 'category':
+                    $this->getCategory();
+                    break;
+                case 'attachment':
+                    $this->getAttachments();
+                    break;
+                case 'supplier':
+                    $this->getSupplier();
+                    break;
+                case  'people':
+                    $this->getPeople();
+                default:
+                    die('Table not implemented');
+            }
+            return $this->column;
         }
-        return $this->column;
     }
 
     /**
@@ -50,7 +58,7 @@ class Models
             "TaxRate" => ['Integer', 'Attribute', 'Required'],
             "UnitPrice" => ['Integer', 'Attribute', 'Required'],
             "MarketingComments" => ['LongText', 'Attribute', 'Nullable'],
-            "CategoryID" => ['Category', 'HasMany', 'Required'],
+            "CategoryID" => ['Category', 'HasOne', 'Required'],
             "LastEditedBy" => ['People', 'HasOne', 'Required'],
         );
     }
@@ -76,4 +84,50 @@ class Models
             "StockItemID" => ['StockItem', 'HasOne', 'Nullable'],
         );
     }
+
+
+    private function getPeople()
+    {
+        $this->column = array(
+            "PeopleID" => ['Integer', 'PrimaryKey', 'Required' ],
+            "FullName" => ['Varchar', 'Attribute', 'Unique'],
+            "LogonName" => ['Supplier', 'HasOne', 'Required'],
+            "HashedPassword" => ['Varchar', 'Attribute', 'Required'],
+            "IsSystemUser" => ['Integer', 'Attribute', 'Required'],
+            "Role" => ['Integer', 'Attribute', 'Required'],
+            "PhoneNumber" => ['Boolean', 'Attribute', 'Required'],
+            "EmailAddress" => ['Varchar', 'Attribute', 'Unique'],
+            "Photo" => ['Blob', 'Attribute', 'Nullable'],
+            "LastEditedBy" => ['People', 'HasOne', 'Nullable'],
+        );
+    }
+    private function getSupplier()
+    {
+        $this->column = array(
+            "SupplierID" => ['Integer', 'PrimaryKey', 'Required' ],
+            "SupplierName" => ['Varchar', 'Attribute', 'Required'],
+            "PrimaryContactPersonID" => ['Integer', 'HasOne', 'Required'],
+            "AlternateContactPersonID" => ['Integer', 'HasOne', 'Required'],
+            "SupplierReference" => ['Varchar', 'Attribute', 'Required'],
+            "InternalComments" => ['LongText', 'Attribute', 'Required'],
+            "WebsiteURL" => ['Boolean', 'Attribute', 'Required'],
+            "LastEditedBy" => ['Varchar', 'Attribute', 'Required'],
+            "AddressID" => ['Integer', 'Attribute', 'Required'],
+
+        );
+    }
+
+    /**
+     * Gets the type of the $key
+     * @param $key
+     * @return mixed
+     */
+    protected function getType($key)
+    {
+        if(array_key_exists($key, $this->column))
+        {
+            return $this->column[$key][0];
+        }
+    }
+
 }
--- Model/Supplier.php
+++ Model/Supplier.php
@@ -5,5 +5,164 @@ namespace Model;
 
 class Supplier extends User
 {
+    private $SupplierID;
+    private $SupplierName;
+    private $PrimaryContactPersonID;
+    private $AlternateContactPersonID;
+    private $SupplierReference;
+    private $InternalComments;
+    private $WebsiteURL;
+    private $LastEditedBy;
+    private $AddressID;
+
+    function __construct()
+    {
+            Parent::__construct();
+            $this->table = "Supplier";
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSupplierID()
+    {
+        return $this->SupplierID;
+    }
+
+    /**
+     * @param mixed $SupplierID
+     */
+    public function setSupplierID($SupplierID)
+    {
+        $this->SupplierID = $SupplierID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSupplierName()
+    {
+        return $this->SupplierName;
+    }
+
+    /**
+     * @param mixed $SupplierName
+     */
+    public function setSupplierName($SupplierName)
+    {
+        $this->SupplierName = $SupplierName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrimaryContactPersonID()
+    {
+        return $this->PrimaryContactPersonID;
+    }
+
+    /**
+     * @param mixed $PrimaryContactPersonID
+     */
+    public function setPrimaryContactPersonID($PrimaryContactPersonID)
+    {
+        $this->PrimaryContactPersonID = $PrimaryContactPersonID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAlternateContactPersonID()
+    {
+        return $this->AlternateContactPersonID;
+    }
+
+    /**
+     * @param mixed $AlternateContactPersonID
+     */
+    public function setAlternateContactPersonID($AlternateContactPersonID)
+    {
+        $this->AlternateContactPersonID = $AlternateContactPersonID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSupplierReference()
+    {
+        return $this->SupplierReference;
+    }
+
+    /**
+     * @param mixed $SupplierReference
+     */
+    public function setSupplierReference($SupplierReference)
+    {
+        $this->SupplierReference = $SupplierReference;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInternalComments()
+    {
+        return $this->InternalComments;
+    }
+
+    /**
+     * @param mixed $InternalComments
+     */
+    public function setInternalComments($InternalComments)
+    {
+        $this->InternalComments = $InternalComments;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWebsiteURL()
+    {
+        return $this->WebsiteURL;
+    }
+
+    /**
+     * @param mixed $WebsiteURL
+     */
+    public function setWebsiteURL($WebsiteURL)
+    {
+        $this->WebsiteURL = $WebsiteURL;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastEditedBy()
+    {
+        return $this->LastEditedBy;
+    }
+
+    /**
+     * @param mixed $LastEditedBy
+     */
+    public function setLastEditedBy($LastEditedBy)
+    {
+        $this->LastEditedBy = $LastEditedBy;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddressID()
+    {
+        return $this->AddressID;
+    }
+
+    /**
+     * @param mixed $AddressID
+     */
+    public function setAddressID($AddressID)
+    {
+        $this->AddressID = $AddressID;
+    }
 
 }
