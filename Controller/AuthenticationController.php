0008a6ee71e1d68a245c4fad76ead56cc2cdcf6d
Sven <pluis29@yahoo.com>
2020-01-08 13:10:08 +0100

    code cleanup

--- Controller/AuthenticationController.php
+++ Controller/AuthenticationController.php
@@ -14,11 +14,24 @@ class AuthenticationController
     public function getdata()
     {
         $getthedata=new Database();
-        //todofixpls Nope! fixing it would over complicate the shit out if it!
         $sqlreturendsomething=$getthedata->selectStmt("SELECT * FROM people WHERE LogonName = '". $this->user->getLogonName()  . "'");
 
             return $sqlreturendsomething;
 
+    }
+
+    public function getCustomerDetails()
+    {
+        $getthedata=new Database();
+        $customerDetails=$getthedata->selectStmt("SELECT * FROM customer WHERE PersonID = '". $_SESSION['USER']['DATA'][0]['PersonID'] . "'");
+        return $customerDetails;
+    }
+
+    public function getAddressDetails()
+    {
+        $getthedata=new Database();
+        $addressDetails=$getthedata->selectStmt("SELECT * FROM address WHERE PersonID = '". $_SESSION['USER']['DATA'][0]['PersonID'] . "'");
+        return $addressDetails;
     }
 
 
@@ -60,11 +73,12 @@ class AuthenticationController
                 $_SESSION['USER']['name']=$this->user->getLogonName();
                 // Place the userdata (an array) into the $_SESSION
                 $_SESSION['USER']['DATA']=$this->getdata();
+                $_SESSION['USER']['CUSTOMER_DETAILS']=$this->getCustomerDetails();
+                $_SESSION['USER']['ADDRESS']=$this->getAddressDetails();
                 // The rest of the userdata.
                 include 'content/frontend/GetUserDetails.php';
                 // Now were done were going back to the index page.
                 $_SESSION['LOGIN_ERROR']=['type'=>'success', 'message'=>'U bent ingelogd'];
-                echo "<pre>";print_r($_SESSION); echo "</pre>";
                 echo "<META HTTP-EQUIV=Refresh CONTENT=\"3;URL=/\">";
             }
             else
